6b7c3059e5117ab6c36bed5395fbd90dd11a6c0d
chanooda <hanrhfqkq@gmail.com>
2025-05-19 21:42:36 +0900

    feat: daily leaderboards 페이지 에러처리

--- app/features/products/pages/daily-leaderboards-page.tsx
+++ app/features/products/pages/daily-leaderboards-page.tsx
@@ -1,4 +1,51 @@
+import { DateTime } from "luxon";
+import { data, isRouteErrorResponse } from "react-router";
+import { z } from "zod";
 import { H2, TypographyLarge } from "~/common/components/ui/typography";
+import type { Route } from "./+types/daily-leaderboards-page";
+
+const paramSchema = z.object({
+  year: z.coerce.number(),
+  month: z.coerce.number(),
+  day: z.coerce.number(),
+});
+
+export function loader({ params }: Route.LoaderArgs) {
+  const { success, data: parsedData } = paramSchema.safeParse(params);
+  if (!success) {
+    throw data(
+      {
+        error_code: "invalid_params",
+        message: "Invalid parameters",
+      },
+      { status: 400 },
+    );
+  }
+
+  const date = DateTime.fromObject(parsedData).setZone("Asia/Seoul");
+  if (!date.isValid) {
+    throw data(
+      {
+        error_code: "invalid_date",
+        message: "Invalid date",
+      },
+      { status: 400 },
+    );
+  }
+
+  const today = DateTime.now().setZone("Asia/Seoul").startOf("day");
+  if (date > today) {
+    throw data(
+      {
+        error_code: "future_date",
+        message: "Future date",
+      },
+      { status: 400 },
+    );
+  }
+
+  return new Response();
+}
 
 export default function DailyLeaderboardsPage() {
   return (
@@ -9,4 +56,18 @@ export default function DailyLeaderboardsPage() {
       </TypographyLarge>
     </div>
   );
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        {error.data.message} / {error.data.error_code}
+      </div>
+    );
+  }
+  if (error instanceof Error) {
+    return <div>{error.message}</div>;
+  }
+  return <div>Unknown Error</div>;
 }
